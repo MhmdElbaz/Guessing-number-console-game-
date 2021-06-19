442a58e39083755c392f9e9fe92466c980ce99d8
Mohamed Elbaz <75902342+MhmdElbaz@users.noreply.github.com>
2021-06-19 21:40:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Stack<int> s = new Stack<int>();
+        s.Push(10);
+        s.Push(20);
+        s.Push(30);
+        s.Push(40);
+        s.Push(50);
+        Console.WriteLine(s.Peek());
+
+    }
+  
+}
+
+
+
+
+
